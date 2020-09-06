03310095610c5696d5016c33fe68620c02c9ff91
María Simó <maria.simo@secuoyas.com>
2020-09-06 15:32:33 +0200

    Add first section basic layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,8 +19,8 @@ const MainTitle = styled.h1`
   font-family: "Savate";
   font-size: 8rem;
   text-transform: uppercase;
-  letter-spacing: -0.5rem;
-  line-height: 1.1;
+  letter-spacing: -0.25rem;
+  line-height: 1.2;
   max-width: 640px;
   margin-left: -7.5rem;
   padding-bottom: 4rem;
@@ -29,6 +29,19 @@ const MainTitle = styled.h1`
     display: flex;
     text-indent: 7.5rem;
   }
+`
+
+const Title = styled.h2`
+  font-family: "Savate";
+  font-size: 6rem;
+  line-height: 1.2;
+`
+
+const Paragraph = styled.p`
+  font-family: "vg5000";
+  font-size: 1.5rem;
+  text-stroke: 0.2rem;
+  line-height: 1.2;
 `
 
 const IndexPage = () => (
@@ -44,7 +57,20 @@ const IndexPage = () => (
         Bamboleo, <span>bambolea</span>
       </MainTitle>
     </Section>
+    <CircleBgSection />
   </Layout>
 )
 
 export default IndexPage
+
+const CircleBgSection = () => (
+  <Section
+    sx={{
+      color: "primary",
+      background: "#000",
+    }}
+  >
+    <Title>Ese amor llega así de esa manera</Title>
+    <Paragraph>No tiene la culpa</Paragraph>
+  </Section>
+)
