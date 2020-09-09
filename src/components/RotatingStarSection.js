27cf58e63773796681dc91f8c012011a8eb8e475
María Simó <maria.simo@secuoyas.com>
2020-09-10 01:05:10 +0200

    Add bamboleo title animating to center

--- src/components/RotatingStarSection.js
+++ src/components/RotatingStarSection.js
@@ -3,6 +3,7 @@ import { jsx } from "theme-ui"
 
 import React, { useRef, useState, useEffect } from "react"
 import { useTransform, useViewportScroll, motion } from "framer-motion"
+import styled from "styled-components"
 
 const RotatingStarSection = () => {
   const ref = useRef()
@@ -12,20 +13,30 @@ const RotatingStarSection = () => {
 
   const y = useTransform(
     scrollY,
-    [elementTop, elementTop + elementHeight / 4],
+    [elementTop, elementTop + elementHeight / 3],
     [elementHeight / 10, 0]
   )
 
   const y2 = useTransform(
     scrollY,
-    [elementTop + elementHeight / 40, elementTop + elementHeight / 2],
+    [elementTop + elementHeight / 40, elementTop + elementHeight / 1.5],
     [elementHeight / 8, 0]
   )
 
   const rotate = useTransform(
     scrollY,
     [elementTop + elementHeight / 2, elementTop + elementHeight],
-    [0, 45]
+    [0, 75]
+  )
+  const x2 = useTransform(
+    scrollY,
+    [elementTop + elementHeight / 2, elementTop + elementHeight],
+    [elementHeight, 30]
+  )
+  const x = useTransform(
+    scrollY,
+    [elementTop + elementHeight / 2, elementTop + elementHeight],
+    [elementHeight * -1, -30]
   )
 
   useEffect(() => {
@@ -47,9 +58,8 @@ const RotatingStarSection = () => {
       <div
         sx={{
           background: "#E79FB9",
-          height: "60vh",
-          width: "60vh",
-
+          height: "700px",
+          width: "700px",
           clipPath: "circle(14.5rem at 50% 50%)",
           position: "absolute",
           top: "0",
@@ -67,8 +77,27 @@ const RotatingStarSection = () => {
           <img src="./thin-star.svg" />
         </motion.div>
       </div>
+      <div>
+        <div sx={{ display: "flex", justifyContent: "center" }}>
+          <Text initial={{ x: "-30vw" }} style={{ x }}>
+            Bamboleo,
+          </Text>
+          <Text initial={{ x: "30vw" }} style={{ x: x2 }}>
+            Bambolea
+          </Text>
+        </div>
+      </div>
     </div>
   )
 }
 
 export default RotatingStarSection
+
+const Text = styled(motion.h2)`
+  color: #e79fb9;
+  font-size: 8rem;
+  font-family: Savate;
+  margin: 0;
+  padding-top: 250px;
+  text-align: center;
+`
